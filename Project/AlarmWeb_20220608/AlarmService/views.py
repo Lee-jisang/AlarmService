d0a8129ade9bce7efc3aee7278a1a6cf35753a04
Lee-jisang <dlwltkd2003@naver.com>
2022-06-08 16:55:12 +0900

    20220608

---
+++ Project/AlarmWeb_20220608/AlarmService/views.py
@@ -0,0 +1,55 @@
+from AlarmService.models import *
+from django.shortcuts import render
+from django.http import StreamingHttpResponse, JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+import AlarmService.sleep
+
+def main(req):
+    context = {
+
+    }
+    return render(req, "main.html", context=context)
+
+def Alarm(req):
+    context = {
+
+    }
+    return render(req, "Alarm.html", context=context)
+
+def StopAlarm(req):
+    context = {
+
+    }
+    return render(req, "StopAlarm.html", context=context)
+
+def about(req):
+    context = {
+
+    }
+    return render(req, "about.html", context=context)
+
+
+@csrf_exempt
+def ajax_connect_config(request):
+    receive_message = request.POST.get('send_data')
+    print(AlarmService.sleep.check)
+    if AlarmService.sleep.check == True:
+        send_message = {'send_data': '0'}
+    else:
+        send_message = {'send_data': '1'}
+    return JsonResponse(send_message)
+
+
+
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def blink_detector(request):
+    return StreamingHttpResponse(gen(AlarmService.sleep.Blink_Detector()),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
